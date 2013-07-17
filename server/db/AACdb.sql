cccd5ade0ff657954efd8566e0f919c7fe17395f
hlaunay <launay.herve@free.fr>
2013-07-17 19:23:16 +0200

    forgotten files :-(

--- server/db/AACdb.sql
+++ server/db/AACdb.sql
@@ -2,10 +2,14 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
+CREATE SCHEMA IF NOT EXISTS `aacdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `aacdb` ;
 
 -- -----------------------------------------------------
 -- Table `aacdb`.`members`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `aacdb`.`members` ;
+
 CREATE  TABLE IF NOT EXISTS `aacdb`.`members` (
   `idmembers` INT NOT NULL ,
   `familyname` VARCHAR(120) NULL ,
@@ -17,9 +21,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `aacdb`.`swipercards`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `aacdb`.`swipercards` ;
+
 CREATE  TABLE IF NOT EXISTS `aacdb`.`swipercards` (
   `idswipercards` INT NOT NULL AUTO_INCREMENT ,
-  `serial` CHAR NULL ,
+  `serial` VARCHAR(15) NULL ,
   PRIMARY KEY (`idswipercards`) )
 ENGINE = InnoDB;
 
@@ -27,22 +33,24 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `aacdb`.`possess`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `aacdb`.`possess` ;
+
 CREATE  TABLE IF NOT EXISTS `aacdb`.`possess` (
   `idpossess` INT NOT NULL AUTO_INCREMENT ,
   `member` INT NULL ,
-  `swipercards` INT NULL ,
+  `swipercard` INT NULL ,
   `validityBegin` DATE NULL ,
   `validityEnd` DATE NULL ,
   PRIMARY KEY (`idpossess`) ,
   INDEX `fk_possess_1` (`member` ASC) ,
-  INDEX `fk_possess_2` (`swipercards` ASC) ,
+  INDEX `fk_possess_2` (`swipercard` ASC) ,
   CONSTRAINT `fk_possess_1`
     FOREIGN KEY (`member` )
     REFERENCES `aacdb`.`members` (`idmembers` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_possess_2`
-    FOREIGN KEY (`swipercards` )
+    FOREIGN KEY (`swipercard` )
     REFERENCES `aacdb`.`swipercards` (`idswipercards` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -52,6 +60,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `aacdb`.`sensors`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `aacdb`.`sensors` ;
+
 CREATE  TABLE IF NOT EXISTS `aacdb`.`sensors` (
   `idsensors` INT NOT NULL AUTO_INCREMENT ,
   `ipaddress` VARCHAR(15) NULL ,
@@ -63,6 +73,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `aacdb`.`rules`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `aacdb`.`rules` ;
+
 CREATE  TABLE IF NOT EXISTS `aacdb`.`rules` (
   `idrules` INT NOT NULL AUTO_INCREMENT ,
   `dtstart` DATETIME NULL ,
@@ -74,33 +86,81 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `aacdb`.`permissions`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `aacdb`.`permissions` ;
+
 CREATE  TABLE IF NOT EXISTS `aacdb`.`permissions` (
   `idpermissions` INT NOT NULL AUTO_INCREMENT ,
   `possess` INT NULL ,
-  `sensors` INT NULL ,
-  `rules` INT NULL ,
+  `sensor` INT NULL ,
+  `rule` INT NULL ,
   PRIMARY KEY (`idpermissions`) ,
   INDEX `fk_permissions_1` (`possess` ASC) ,
-  INDEX `fk_permissions_2` (`rules` ASC) ,
-  INDEX `fk_permissions_3` (`sensors` ASC) ,
+  INDEX `fk_permissions_2` (`rule` ASC) ,
+  INDEX `fk_permissions_3` (`sensor` ASC) ,
   CONSTRAINT `fk_permissions_1`
     FOREIGN KEY (`possess` )
     REFERENCES `aacdb`.`possess` (`idpossess` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_permissions_2`
-    FOREIGN KEY (`rules` )
+    FOREIGN KEY (`rule` )
     REFERENCES `aacdb`.`rules` (`idrules` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_permissions_3`
-    FOREIGN KEY (`sensors` )
+    FOREIGN KEY (`sensor` )
     REFERENCES `aacdb`.`sensors` (`idsensors` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Placeholder table for view `aacdb`.`active_members`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `aacdb`.`active_members` (`idpossess` INT, `firstname` INT, `familyname` INT, `serial` INT, `validityBegin` INT, `validityEnd` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `aacdb`.`active_rules`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `aacdb`.`active_rules` (`firstname` INT, `familyname` INT, `serial` INT, `ipaddress` INT, `location` INT, `dtstart` INT, `dtend` INT);
+
+-- -----------------------------------------------------
+-- View `aacdb`.`active_members`
+-- -----------------------------------------------------
+DROP VIEW IF EXISTS `aacdb`.`active_members` ;
+DROP TABLE IF EXISTS `aacdb`.`active_members`;
+USE `aacdb`;
+CREATE  OR REPLACE VIEW `aacdb`.`active_members` AS
+    SELECT idpossess, firstname, familyname, serial, validityBegin, validityEnd 
+    FROM members, possess, swipercards
+    WHERE members.idmembers=possess.member 
+        AND swipercards.idswipercards=possess.swipercard
+        AND validityBegin<=NOW()
+        AND validityEnd>=NOW();
+
+-- -----------------------------------------------------
+-- View `aacdb`.`active_rules`
+-- -----------------------------------------------------
+DROP VIEW IF EXISTS `aacdb`.`active_rules` ;
+DROP TABLE IF EXISTS `aacdb`.`active_rules`;
+USE `aacdb`;
+CREATE  OR REPLACE VIEW `aacdb`.`active_rules` AS
+SELECT firstname, familyname, serial, ipaddress, location, dtstart, dtend 
+    FROM active_members, permissions, sensors, rules
+    WHERE active_members.idpossess=permissions.possess
+        AND sensors.idsensors=permissions.sensor
+        AND rules.idrules=permissions.rule;
+
+CREATE USER `aac` IDENTIFIED BY '###epitech###';
+
+grant ALL on TABLE `aacdb`.`members` to aac;
+grant ALL on TABLE `aacdb`.`permissions` to aac;
+grant ALL on TABLE `aacdb`.`possess` to aac;
+grant ALL on TABLE `aacdb`.`rules` to aac;
+grant ALL on TABLE `aacdb`.`sensors` to aac;
+grant ALL on TABLE `aacdb`.`swipercards` to aac;
+grant ALL on TABLE `aacdb`.`active_members` to aac;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- server/db/README.md
+++ server/db/README.md
@@ -10,33 +10,14 @@ Database for AAC
 Installation
 ------------
 ### Linux
-Mysql connection
 
-	$ mysql -u root -p
+	$./install.sh
 
-Database creation
-
-	> CREATE DATABASE aacdb;
-
-User creation
-	
-	> CREATE USER "aac"@"localhost";
-
-Set the user password
-
-	> SET password FOR "aac"@"localhost" = password('XXXX');
-
-Give all right on the created database to the created user
-
-	> GRANT ALL ON aacdb.* TO "aac"@"localhost";
-	> QUIT
-
-Create the database structure
-
-	$ mysql -u aac -p aacdb < AACdb.sql
+Data test are available
+	$./install.sh -t
 
 ### Windows
-TODO
+To Be Defined
 
 Test
 -----
